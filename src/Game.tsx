506487b7fbf0eb908fa28254ac69f18613c08ed7
AndyLnd <andy@wire.com>
2021-02-11 09:23:56 +0100

    add github link

--- src/Game.tsx
+++ src/Game.tsx
@@ -9,6 +9,7 @@ import {GameContext} from './GameProvider';
 import SolveBackground from './SolveBackground';
 import FullScreenCenter from './FullScreenCenter';
 import Finish from './Finish';
+import Octocat from './Octocat';
 
 export default () => {
   const {isSolved, isStarted, introMap, map, rotateTile, width, height, reset, tileSize, showBackground} = useContext(
@@ -38,6 +39,7 @@ export default () => {
         <FullScreenCenter css={scaleInTransition}>
           <Board map={introMap} rotateTile={rotateTile} width={4} height={4} tileSize={50} />
           <LevelSelect css={scaleInTransition} />
+          <Octocat />
         </FullScreenCenter>
       </CSSTransition>
     </React.Fragment>
